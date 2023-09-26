c4dfe86d4e5b0e42cd558b492e20a14d3c540c83
ddcas9 <ddcas9@users.noreply.github.com>
2023-09-26 05:52:25 -0400

    fixed package name for 0.1.6

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='neap',
-    version='0.1.5',
+    version='0.1.6s',
     description='A Python wrapper for the NEAP API',
     author='Bard',
     author_email='bard@google.com',
+    packages = ['neap'],
     install_requires=['requests'],
 )
