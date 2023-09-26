907977c8b38d3e28a51eaffa18d9f1b73e25f978
ddcas9 <ddcas9@users.noreply.github.com>
2023-09-26 06:46:27 -0400

    version 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='neap',
-    version='0.1.8',
+    version='0.1.9',
     description='A Python wrapper for the NEAP API',
     author='Bard',
     author_email='bard@google.com',
