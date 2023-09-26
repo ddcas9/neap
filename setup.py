668fa26fcea056b49632878ff5101ecc366e64e9
ddcas9 <ddcas9@users.noreply.github.com>
2023-09-25 21:26:59 -0400

    change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='neap',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python wrapper for the NEAP API',
     author='Bard',
     author_email='bard@google.com',
