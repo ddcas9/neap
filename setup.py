770d7ae9a70ad121ba2f460d3a836099415603a5
ddcas9 <ddcas9@users.noreply.github.com>
2023-09-25 21:30:33 -0400

    final patch for import issue with Requests due to pymodule naming in setup

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 
 setup(
     name='neap',
-    version='0.1.2',
+    version='0.1.3',
     description='A Python wrapper for the NEAP API',
     author='Bard',
     author_email='bard@google.com',
     packages=['neap'],
-    py_modules=['neap.ReportCard'],
     install_requires=['requests'],
 )
